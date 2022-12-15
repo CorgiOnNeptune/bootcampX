195fdcdafed029ab2856822b63ea77f08cb0098b
Justin <justinjdbennett@gmail.com>
2022-12-15 06:34:30 +0000

    paramaterize queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,18 +11,26 @@ const pool = new Pool({
   port: env.PORT || 8001
 });
 
-const args = process.argv;
+// Our (devs) input that we know we have control over
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id AS student_id, students.name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[2]}%'
-LIMIT ${args[3] || 5};
-`)
+// User input that could be malicious
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+
+// Paramaterized query, safe from SQL injection
+pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`);
+    res.rows.forEach(row => {
+      console.log(`${row.name} has an id of ${row.student_id} and was in the ${row.cohort} cohort.`);
     })
   })
   .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,18 +11,21 @@ const pool = new Pool({
   port: env.PORT || 8001
 });
 
-const args = process.argv;
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher,
+    cohorts.name AS cohort
+  FROM assistance_requests
+    JOIN teachers ON teachers.id = teacher_id
+    JOIN students ON students.id = student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher;
+  `;
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher,
-  cohorts.name AS cohort
-FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
